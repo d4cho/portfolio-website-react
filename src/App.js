716aae1eecbe902c8f380b226c1f9de130a937da
Daniel Cho <danielcho91@gmail.com>
2020-11-16 17:48:30 -0500

    routes and links

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Sidebar from './Components/Sidebar/Sidebar';
+import HomePage from './Components/HomePage/HomePage';
+import AboutPage from './Components/AboutPage/AboutPage';
+import SkillsPage from './Components/SkillsPage/SkillsPage';
+import ProjectsPage from './Components/ProjectsPage/ProjectsPage';
+import ContactPage from './Components/ContactPage/ContactPage';
+import LoadingPage from './Components/LoadingPage/LoadingPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Sidebar />
       <Switch>
-        <Route />
-        <Route />
-        <Route />
-        <Route />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/about' component={AboutPage} />
+        <Route exact path='/skills' component={SkillsPage} />
+        <Route exact path='/projects' component={ProjectsPage} />
+        <Route exact path='/contact' component={ContactPage} />
+        <Route exact path='/loading' component={LoadingPage} />
       </Switch>
     </BrowserRouter>
   );
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Sidebar.css';
+import { Link } from 'react-router-dom';
 
 import {
   AiOutlineHome,
@@ -20,27 +20,38 @@ const Sidebar = () => {
   return (
     <>
       <div className='Sidebar'>
+        <img className='Logo' src={logo} />
         <div className='Icon-container'>
-          <div className='Icon-style'>
-            <AiOutlineHome style={{ fontSize: '50px' }} />
-            <span>HOME</span>
-          </div>
-          <div className='Icon-style'>
-            <AiOutlineUser style={{ fontSize: '50px' }} />
-            <span>ABOUT</span>
-          </div>
-          <div className='Icon-style'>
-            <AiOutlineUnorderedList style={{ fontSize: '50px' }} />
-            <span>SKILLS</span>
-          </div>
-          <div className='Icon-style'>
-            <AiOutlineAppstore style={{ fontSize: '50px' }} />
-            <span>MY WORK</span>
-          </div>
-          <div className='Icon-style'>
-            <AiOutlineMail style={{ fontSize: '50px' }} />
-            <span>CONTACT</span>
-          </div>
+          <Link to='/' style={{ textDecoration: 'none' }}>
+            <div className='Icon-style'>
+              <AiOutlineHome style={{ fontSize: '50px' }} />
+              <span>HOME</span>
+            </div>
+          </Link>
+          <Link to='/about' style={{ textDecoration: 'none' }}>
+            <div className='Icon-style'>
+              <AiOutlineUser style={{ fontSize: '50px' }} />
+              <span>ABOUT</span>
+            </div>
+          </Link>
+          <Link to='/skills' style={{ textDecoration: 'none' }}>
+            <div className='Icon-style'>
+              <AiOutlineUnorderedList style={{ fontSize: '50px' }} />
+              <span>SKILLS</span>
+            </div>
+          </Link>
+          <Link to='/projects' style={{ textDecoration: 'none' }}>
+            <div className='Icon-style'>
+              <AiOutlineAppstore style={{ fontSize: '50px' }} />
+              <span>PROJECTS</span>
+            </div>
+          </Link>
+          <Link to='/contact' style={{ textDecoration: 'none' }}>
+            <div className='Icon-style'>
+              <AiOutlineMail style={{ fontSize: '50px' }} />
+              <span>CONTACT</span>
+            </div>
+          </Link>
         </div>
         <div className='Burger-menu'>
           <AiOutlineMenu
