a34bd579b36747c39b4e5da6ac068f7a19ed2fcd
Daniel Cho <danielcho91@gmail.com>
2020-11-18 14:01:51 -0500

    icon name change on hover

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -17,9 +17,42 @@ const ICON_SIZE = {
 
 const Sidebar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
+  const [isHomeHover, setIsHomeHover] = useState(false);
+  const [isAboutHover, setIsAboutHover] = useState(false);
+  const [isSkillsHover, setIsSkillsHover] = useState(false);
+  const [isProjectsHover, setIsProjectsHover] = useState(false);
+  const [isContactHover, setIsContactHover] = useState(false);
 
   const showNavbarHandler = () => {
     setShowNavbar(!showNavbar);
+  };
+
+  const iconHoverTrueHandler = (e) => {
+    if (e.currentTarget.id === 'home') {
+      setIsHomeHover(true);
+    } else if (e.currentTarget.id === 'about') {
+      setIsAboutHover(true);
+    } else if (e.currentTarget.id === 'skills') {
+      setIsSkillsHover(true);
+    } else if (e.currentTarget.id === 'projects') {
+      setIsProjectsHover(true);
+    } else if (e.currentTarget.id === 'contact') {
+      setIsContactHover(true);
+    }
+  };
+
+  const iconHoverFalseHandler = (e) => {
+    if (e.currentTarget.id === 'home') {
+      setIsHomeHover(false);
+    } else if (e.currentTarget.id === 'about') {
+      setIsAboutHover(false);
+    } else if (e.currentTarget.id === 'skills') {
+      setIsSkillsHover(false);
+    } else if (e.currentTarget.id === 'projects') {
+      setIsProjectsHover(false);
+    } else if (e.currentTarget.id === 'contact') {
+      setIsContactHover(false);
+    }
   };
 
   return (
@@ -28,33 +61,64 @@ const Sidebar = () => {
         <img className='Logo' src={logo} />
         <div className='Icon-container'>
           <Link to='/' style={{ textDecoration: 'none' }}>
-            <div className='Icon-style'>
-              <AiOutlineHome style={ICON_SIZE} />
-              <span>HOME</span>
+            <div
+              className='Icon-style'
+              onMouseEnter={iconHoverTrueHandler}
+              onMouseLeave={iconHoverFalseHandler}
+              id='home'>
+              {isHomeHover ? <p>HOME</p> : <AiOutlineHome style={ICON_SIZE} />}
             </div>
           </Link>
           <Link to='/about' style={{ textDecoration: 'none' }}>
-            <div className='Icon-style'>
-              <AiOutlineUser style={ICON_SIZE} />
-              <span>ABOUT</span>
+            <div
+              className='Icon-style'
+              onMouseEnter={iconHoverTrueHandler}
+              onMouseLeave={iconHoverFalseHandler}
+              id='about'>
+              {isAboutHover ? (
+                <p>ABOUT</p>
+              ) : (
+                <AiOutlineUser style={ICON_SIZE} />
+              )}
             </div>
           </Link>
           <Link to='/skills' style={{ textDecoration: 'none' }}>
-            <div className='Icon-style'>
-              <AiOutlineUnorderedList style={ICON_SIZE} />
-              <span>SKILLS</span>
+            <div
+              className='Icon-style'
+              onMouseEnter={iconHoverTrueHandler}
+              onMouseLeave={iconHoverFalseHandler}
+              id='skills'>
+              {isSkillsHover ? (
+                <p>SKILLS</p>
+              ) : (
+                <AiOutlineUnorderedList style={ICON_SIZE} />
+              )}
             </div>
           </Link>
           <Link to='/projects' style={{ textDecoration: 'none' }}>
-            <div className='Icon-style'>
-              <AiOutlineAppstore style={ICON_SIZE} />
-              <span>PROJECTS</span>
+            <div
+              className='Icon-style'
+              onMouseEnter={iconHoverTrueHandler}
+              onMouseLeave={iconHoverFalseHandler}
+              id='projects'>
+              {isProjectsHover ? (
+                <p>PROJECTS</p>
+              ) : (
+                <AiOutlineAppstore style={ICON_SIZE} />
+              )}
             </div>
           </Link>
           <Link to='/contact' style={{ textDecoration: 'none' }}>
-            <div className='Icon-style'>
-              <AiOutlineMail style={ICON_SIZE} />
-              <span>CONTACT</span>
+            <div
+              className='Icon-style'
+              onMouseEnter={iconHoverTrueHandler}
+              onMouseLeave={iconHoverFalseHandler}
+              id='contact'>
+              {isContactHover ? (
+                <p>CONTACT</p>
+              ) : (
+                <AiOutlineMail style={ICON_SIZE} />
+              )}
             </div>
           </Link>
         </div>
@@ -66,33 +130,68 @@ const Sidebar = () => {
         <div className='Navbar'>
           <div className='Icon-container-menu'>
             <Link to='/' style={{ textDecoration: 'none' }}>
-              <div className='Icon-style'>
-                <AiOutlineHome style={ICON_SIZE} />
-                <span>HOME</span>
+              <div
+                className='Icon-style'
+                onMouseEnter={iconHoverTrueHandler}
+                onMouseLeave={iconHoverFalseHandler}
+                id='home'>
+                {isHomeHover ? (
+                  <p>HOME</p>
+                ) : (
+                  <AiOutlineHome style={ICON_SIZE} />
+                )}
               </div>
             </Link>
             <Link to='/about' style={{ textDecoration: 'none' }}>
-              <div className='Icon-style'>
-                <AiOutlineUser style={ICON_SIZE} />
-                <span>ABOUT</span>
+              <div
+                className='Icon-style'
+                onMouseEnter={iconHoverTrueHandler}
+                onMouseLeave={iconHoverFalseHandler}
+                id='about'>
+                {isAboutHover ? (
+                  <p>ABOUT</p>
+                ) : (
+                  <AiOutlineUser style={ICON_SIZE} />
+                )}
               </div>
             </Link>
             <Link to='/skills' style={{ textDecoration: 'none' }}>
-              <div className='Icon-style'>
-                <AiOutlineUnorderedList style={ICON_SIZE} />
-                <span>SKILLS</span>
+              <div
+                className='Icon-style'
+                onMouseEnter={iconHoverTrueHandler}
+                onMouseLeave={iconHoverFalseHandler}
+                id='skills'>
+                {isSkillsHover ? (
+                  <p>SKILLS</p>
+                ) : (
+                  <AiOutlineUnorderedList style={ICON_SIZE} />
+                )}
               </div>
             </Link>
             <Link to='/projects' style={{ textDecoration: 'none' }}>
-              <div className='Icon-style'>
-                <AiOutlineAppstore style={ICON_SIZE} />
-                <span>PROJECTS</span>
+              <div
+                className='Icon-style'
+                onMouseEnter={iconHoverTrueHandler}
+                onMouseLeave={iconHoverFalseHandler}
+                id='projects'>
+                {isProjectsHover ? (
+                  <p>PROJECTS</p>
+                ) : (
+                  <AiOutlineAppstore style={ICON_SIZE} />
+                )}
               </div>
             </Link>
             <Link to='/contact' style={{ textDecoration: 'none' }}>
-              <div className='Icon-style'>
-                <AiOutlineMail style={ICON_SIZE} />
-                <span>CONTACT</span>
+              <div
+                className='Icon-style'
+                onMouseEnter={iconHoverTrueHandler}
+                onMouseLeave={iconHoverFalseHandler}
+                id='contact'>
+                {isContactHover ? (
+                  <p>CONTACT</p>
+                ) : (
+                  <AiOutlineMail style={ICON_SIZE} />
+                )}
               </div>
             </Link>
           </div>
