cacf6a4f0ede374c7a9846e1818a5b931920fc99
Daniel Cho <danielcho91@gmail.com>
2020-11-20 14:39:44 -0500

    contact page layout

--- src/Components/ContactPage/ContactPage.js
+++ src/Components/ContactPage/ContactPage.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+import Form from './sections/Form';
+
 const ContactPage = () => {
-  return <div>ContactPage ContactPage</div>;
+  return (
+    <div className='Background-contact-page'>
+      <div className='Content-contact-page'>
+        <span className='Title-contact-page'>
+          <span className='Text-animated-contact-page'>C</span>
+          <span className='Text-animated-contact-page'>o</span>
+          <span className='Text-animated-contact-page'>n</span>
+          <span className='Text-animated-contact-page'>t</span>
+          <span className='Text-animated-contact-page'>a</span>
+          <span className='Text-animated-contact-page'>c</span>
+          <span className='Text-animated-contact-page'>t</span>
+          <span className='Text-animated-contact-page'>&nbsp;</span>
+          <span className='Text-animated-contact-page'>m</span>
+          <span className='Text-animated-contact-page'>e</span>
+        </span>
+        <p className='Blurb-contact-page'>
+          Feel free to contact me at danielcho91@gmail.com or call me at
+          647-323-1202. If you have other requests or questions, don't hesitate
+          to contact me using the form below.
+        </p>
+        <div>
+          <Form />
+        </div>
+      </div>
+      <div className='Map-contact-page'>map</div>
+    </div>
+  );
 };
 
 export default ContactPage;
